7505d3ce2ec5f574b5ba930bfd29abba9408b437
gaurava16fc <gaurava16fc@gmail.com>
2020-07-04 11:26:20 +0530

    Code cleaning done

--- Employee-SPA/src/app/_services/error.interceptor.ts
+++ Employee-SPA/src/app/_services/error.interceptor.ts
@@ -1,18 +1,18 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import {
   HttpInterceptor,
   HttpErrorResponse,
   HTTP_INTERCEPTORS,
-} from "@angular/common/http";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+} from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
   intercept(
-    req: import("@angular/common/http").HttpRequest<any>,
-    next: import("@angular/common/http").HttpHandler
-  ): import("rxjs").Observable<import("@angular/common/http").HttpEvent<any>> {
+    req: import('@angular/common/http').HttpRequest<any>,
+    next: import('@angular/common/http').HttpHandler
+  ): import('rxjs').Observable<import('@angular/common/http').HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error) => {
         if (error.status === 401 || error.status === 403) {
@@ -20,16 +20,16 @@ export class ErrorInterceptor implements HttpInterceptor {
         }
 
         if (error instanceof HttpErrorResponse) {
-          const applicationError = error.headers.get("Application-Error");
+          const applicationError = error.headers.get('Application-Error');
           if (applicationError) {
             return throwError(applicationError);
           }
           const serverError = error.error;
-          let modalStateErrors = "";
-          if (serverError.errors && typeof serverError.errors === "object") {
+          let modalStateErrors = '';
+          if (serverError.errors && typeof serverError.errors === 'object') {
             for (const key in serverError.errors) {
               if (serverError.errors[key]) {
-                modalStateErrors += serverError.errors[key] + "\n";
+                modalStateErrors += serverError.errors[key] + '\n';
               }
             }
           }
