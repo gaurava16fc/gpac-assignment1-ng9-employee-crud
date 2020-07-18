8c6c927e7d38342a5f5f10e3f3de63804272f782
gaurava16fc <gaurava16fc@gmail.com>
2020-07-18 18:55:21 +0530

    Upgraded Angular-CLI-9 to Angular-CLI-10 and also updated the references to this project.

--- Employee.API/Models/Employee.cs
+++ Employee.API/Models/Employee.cs
@@ -22,19 +22,33 @@ namespace EmployeeApp.API.Models
         [Required(ErrorMessage = "Gender is a required field")]
         public string Gender { get; set; }
 
+        [Required(ErrorMessage = "Email is a required field")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is a required field")]
         public string PhoneNumber { get; set; }
 
+        [Required(ErrorMessage = "Date Of Birth is a required field")]
         public DateTime DateOfBirth { get; set; }
+
         public DateTime? DateOfJoining { get; set; }
+
         public Department Department { get; set; }
+
         public int DepartmentId { get; set; }
+
         public Facility Facility { get; set; }
+
         public int FacilityId { get; set; }
-        public string City { get; set; }        
+
+        public string City { get; set; }
+        
         public string Country { get; set; }
+
         public ICollection<Photo> Photos { get; set; }
+
         public DateTime CreatedOn { get; set; }
+
         public DateTime? ModifiedOn { get; set; }
     }
 }
